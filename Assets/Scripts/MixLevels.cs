408455b95126ce8fc1fa5fb87335de9347dd7b24
kkgm13 <kkgm013@gmail.com>
2018-03-23 16:41:09 +0000

    Massive Changes and Additions

---
+++ Assets/Scripts/MixLevels.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class MixLevels : MonoBehaviour {
+
+	public AudioMixer masterMix;
+	 
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	//Set the Effect Music Level
+	public void setEffectsLvl(float effectLvl){
+		masterMix.SetFloat ("effectMusic", effectLvl);
+	}
+
+	//Set the Background Music Level
+	public void setBackMusicLvl (float musicLvl){
+		masterMix.SetFloat ("backgroundMusic", musicLvl);
+	}
+
+}
